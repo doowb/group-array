032b348b8b0875e45244b98529fa8d2b670fac69
Brian Woodward <brian.woodward@gmail.com>
2015-07-19 13:55:20 -0400

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * group-array <https://github.com/doowb/group-array>
+ *
+ * Copyright (c) 2015, Brian Woodward.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
