d7fefae7e9c429f426262e39acdb9735cbac666f
jonschlinkert <jon.schlinkert@sellside.com>
2017-02-24 09:35:57 -0500

    lint, add unit test

--- test/fixtures/tags-array.js
+++ test/fixtures/tags-array.js
@@ -1,47 +1,89 @@
 module.exports = [
-  { categories: { one: [ 'A' ] },
-  name: 'Post 1',
-  content: 'Post 1',
-  key: 'post-1.md',
-  tags: [ 'A' ] },
-{ categories: { one: [ 'A' ], two: [ 'B', 'C' ] },
-  name: 'Post 2',
-  content: 'Post 2',
-  key: 'post-2.md',
-  tags: [ 'A', 'B', 'C' ] },
-{ categories: { one: [ 'B' ], two: [ 'C', 'D' ] },
-  name: 'Post 3',
-  content: 'Post 3',
-  key: 'post-3.md',
-  tags: [ 'B', 'C', 'D' ] },
-{ categories: { one: [ 'A' ], two: [ 'B', 'C' ] },
-  name: 'Post 2',
-  content: 'Post 2',
-  key: 'post-2.md',
-  tags: [ 'A', 'B', 'C' ] },
-{ categories: { one: [ 'B' ], two: [ 'C', 'D' ] },
-  name: 'Post 3',
-  content: 'Post 3',
-  key: 'post-3.md',
-  tags: [ 'B', 'C', 'D' ] },
-{ categories: { three: [ 'B' ], four: [ 'E', 'F', 'G' ] },
-  name: 'Post 4',
-  content: 'Post 4',
-  key: 'post-4.md',
-  tags: [ 'B', 'E', 'F', 'G' ] },
-{ categories: { three: [ 'B' ], four: [ 'E', 'F', 'G' ] },
-  name: 'Post 4',
-  content: 'Post 4',
-  key: 'post-4.md',
-  tags: [ 'B', 'E', 'F', 'G' ] },
-{ categories: { four: [ 'C', 'F' ] },
-  name: 'Post 5',
-  content: 'Post 5',
-  key: 'post-5.md',
-  tags: [ 'C', 'F' ] },
-{ categories: { four: [ 'F', 'G' ] },
-  name: 'Post 6',
-  content: 'Post 6',
-  key: 'post-6.md',
-  tags: [ 'F', 'G' ] }
+  {
+    categories: {
+      one: ['A']
+    },
+    name: 'Post 1',
+    content: 'Post 1',
+    key: 'post-1.md',
+    tags: ['A']
+  },
+  {
+    categories: {
+      one: ['A'],
+      two: ['B', 'C']
+    },
+    name: 'Post 2',
+    content: 'Post 2',
+    key: 'post-2.md',
+    tags: ['A', 'B', 'C']
+  },
+  {
+    categories: {
+      one: ['B'],
+      two: ['C', 'D']
+    },
+    name: 'Post 3',
+    content: 'Post 3',
+    key: 'post-3.md',
+    tags: ['B', 'C', 'D']
+  },
+  {
+    categories: {
+      one: ['A'],
+      two: ['B', 'C']
+    },
+    name: 'Post 2',
+    content: 'Post 2',
+    key: 'post-2.md',
+    tags: ['A', 'B', 'C']
+  },
+  {
+    categories: {
+      one: ['B'],
+      two: ['C', 'D']
+    },
+    name: 'Post 3',
+    content: 'Post 3',
+    key: 'post-3.md',
+    tags: ['B', 'C', 'D']
+  },
+  {
+    categories: {
+      three: ['B'],
+      four: ['E', 'F', 'G']
+    },
+    name: 'Post 4',
+    content: 'Post 4',
+    key: 'post-4.md',
+    tags: ['B', 'E', 'F', 'G']
+  },
+  {
+    categories: {
+      three: ['B'],
+      four: ['E', 'F', 'G']
+    },
+    name: 'Post 4',
+    content: 'Post 4',
+    key: 'post-4.md',
+    tags: ['B', 'E', 'F', 'G']
+  },
+  {
+    categories: {
+      four: ['C', 'F']
+    },
+    name: 'Post 5',
+    content: 'Post 5',
+    key: 'post-5.md',
+    tags: ['C', 'F']
+  },
+  {
+    categories: {
+      four: ['F', 'G']
+    },
+    name: 'Post 6',
+    content: 'Post 6',
+    key: 'post-6.md',
+    tags: ['F', 'G']
+  }
 ];
--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 'use strict';
 
-/* deps: mocha */
+require('mocha');
 var util = require('util');
 var assert = require('assert');
 var union = require('union-value');
@@ -9,24 +9,24 @@ var forOwn = require('for-own');
 var groupArray = require('../');
 require('should');
 
-describe('errors', function () {
-  it('should throw an error when invalid args are passed:', function () {
-    (function () {
+describe('errors', function() {
+  it('should throw an error when invalid args are passed:', function() {
+    (function() {
       groupArray('');
     }).should.throw('group-array expects an array.');
   });
 });
 
-describe('group-array', function () {
-  it('should return an empty array when an empty array is passed:', function () {
+describe('group-array', function() {
+  it('should return an empty array when an empty array is passed:', function() {
     groupArray([]).should.eql([]);
   });
 
-  it('should return the original array when no grouping properties are passed:', function () {
+  it('should return the original array when no grouping properties are passed:', function() {
     groupArray(['a', 'b', 'c']).should.eql(['a', 'b', 'c']);
   });
 
-  it('should create groups based on the value of the specified property', function () {
+  it('should create groups based on the value of the specified property', function() {
     var arr = [
       {tag: 'one', content: 'A'},
       {tag: 'one', content: 'B'},
@@ -54,7 +54,7 @@ describe('group-array', function () {
     });
   });
 
-  it('should support passing the property as an array:', function () {
+  it('should support passing the property as an array:', function() {
     var arr = [
       {tag: 'one', content: 'A'},
       {tag: 'one', content: 'B'},
@@ -82,7 +82,7 @@ describe('group-array', function () {
     });
   });
 
-  it('should create groups based on the value of nested properties', function () {
+  it('should create groups based on the value of nested properties', function() {
     var arr = [
       { data: { tag: 'one' }, content: 'A'},
       { data: { tag: 'one' }, content: 'B'},
@@ -138,7 +138,7 @@ describe('group-array', function () {
     });
   });
 
-  it('should create groups from properties with object values:', function () {
+  it('should create groups from properties with object values:', function() {
     var arr = [
       { data: { categories: { one: [ 'one' ], four: ['five', 'six'] }}, content: 'A'},
       { data: { categories: { one: [ 'one' ] }}, content: 'B'},
@@ -149,12 +149,12 @@ describe('group-array', function () {
     ];
 
     function getChildren(prop) {
-      return function (obj) {
+      return function(obj) {
         var val = get(obj, prop);
         var self = this;
 
-        forOwn(val, function (arr) {
-          arr.forEach(function (key) {
+        forOwn(val, function(arr) {
+          arr.forEach(function(key) {
             union(self, key, [obj]);
           });
         });
@@ -165,7 +165,7 @@ describe('group-array', function () {
     assert.deepEqual(Object.keys(actual), ['one', 'four', 'two']);
   });
 
-  it('should create groups and ignore objects without matching property values:', function () {
+  it('should create groups and ignore objects without matching property values:', function() {
     var arr = [
       {tag: 'one', content: 'A'},
       {content: 'B'},
@@ -190,39 +190,54 @@ describe('group-array', function () {
     });
   });
 
-  it('should support properties with array values:', function () {
+  it('should support properties with array values:', function() {
     var fixture = require('./fixtures/tags-array.js');
     var actual = groupArray(fixture, 'tags');
     assert.deepEqual(actual, require('./expected/tags-array.js'));
   });
 
-  it('should group nested properties with array values that are created by a function:', function () {
+  it('should group nested properties with array values that are created by a function:', function() {
     var fixture = require('./fixtures/categories-tags.js');
-    var actual = groupArray(fixture, 'categories', function  (obj, key) {
-      return obj.categories[key];
+    var actual = groupArray(fixture, 'categories', function(val, key) {
+      return val.categories[key];
     });
     assert.deepEqual(actual, require('./expected/categories-tags.js'));
   });
 
-  it('should create multiple, nested groups:', function () {
+  it('should create multiple, nested groups:', function() {
     var fixture = require('./fixtures/nested.js');
     var actual = groupArray(fixture, 'data.tag', 'data.month');
     assert.deepEqual(actual, require('./expected/nested.js'));
   });
 
-  it('should create multiple, deeply nested groups:', function () {
+  it('should create multiple, deeply nested groups:', function() {
     var fixture = require('./fixtures/deeply-nested.js');
     var actual = groupArray(fixture, 'data.tag', 'data.month', 'data.day');
     assert.deepEqual(actual, require('./expected/deeply-nested.js'));
   });
 
-  it('should create multiple, insanely nested groups:', function () {
+  it('should use a function create nested groups for multiple properties:', function() {
+    var fixture = require('./fixtures/date-function.js');
+    var actual = groupArray(fixture, 'data.tag', function(obj) {
+      var date = obj.data.date.split('-');
+      obj.data.month = date[0];
+      obj.data.day = date[1];
+      delete obj.data.date;
+      return obj.data.month;
+    }, function(obj) {
+      return obj.data.day;
+    });
+
+    assert.deepEqual(actual, require('./expected/deeply-nested.js'));
+  });
+
+  it('should create multiple, insanely nested groups:', function() {
     var fixture = require('./fixtures/insanely-nested.js');
     var actual = groupArray(fixture, 'data.year', 'data.tag', 'data.month', 'data.day');
     assert.deepEqual(actual, require('./expected/insanely-nested.js'));
   });
 
-  it('should support properties as an array:', function () {
+  it('should support properties as an array:', function() {
     var fixture = require('./fixtures/insanely-nested.js');
     var actual = groupArray(fixture, ['data.year', 'data.tag', 'data.month', 'data.day']);
     assert.deepEqual(actual, require('./expected/insanely-nested.js'));
