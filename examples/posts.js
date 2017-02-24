ca89cab4b1359e746abddace6964a389856f0394
jonschlinkert <jon.schlinkert@sellside.com>
2017-02-24 09:37:23 -0500

    add example, update readme examples

---
+++ examples/posts.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var util = require('util');
+var groupBy = require('..');
+
+var arr = [
+  { data: { year: '2016', tag: 'one', month: 'jan', day: '01'}, content: '...'},
+  { data: { year: '2016', tag: 'one', month: 'jan', day: '01'}, content: '...'},
+  { data: { year: '2016', tag: 'one', month: 'jan', day: '02'}, content: '...'},
+  { data: { year: '2016', tag: 'one', month: 'jan', day: '02'}, content: '...'},
+  { data: { year: '2016', tag: 'one', month: 'feb', day: '10'}, content: '...'},
+  { data: { year: '2016', tag: 'one', month: 'feb', day: '10'}, content: '...'},
+  { data: { year: '2016', tag: 'one', month: 'feb', day: '12'}, content: '...'},
+  { data: { year: '2016', tag: 'one', month: 'feb', day: '12'}, content: '...'},
+  { data: { year: '2016', tag: 'two', month: 'jan', day: '14'}, content: '...'},
+  { data: { year: '2016', tag: 'two', month: 'jan', day: '14'}, content: '...'},
+  { data: { year: '2016', tag: 'two', month: 'jan', day: '16'}, content: '...'},
+  { data: { year: '2016', tag: 'two', month: 'jan', day: '16'}, content: '...'},
+  { data: { year: '2016', tag: 'two', month: 'feb', day: '18'}, content: '...'},
+  { data: { year: '2017', tag: 'two', month: 'feb', day: '18'}, content: '...'},
+  { data: { year: '2017', tag: 'two', month: 'feb', day: '10'}, content: '...'},
+  { data: { year: '2017', tag: 'two', month: 'feb', day: '10'}, content: '...'},
+  { data: { year: '2017', tag: 'three', month: 'jan', day: '01'}, content: '...'},
+  { data: { year: '2017', tag: 'three', month: 'jan', day: '01'}, content: '...'},
+  { data: { year: '2017', tag: 'three', month: 'jan', day: '02'}, content: '...'},
+  { data: { year: '2017', tag: 'three', month: 'jan', day: '02'}, content: '...'},
+  { data: { year: '2017', tag: 'three', month: 'feb', day: '01'}, content: '...'},
+  { data: { year: '2017', tag: 'three', month: 'feb', day: '01'}, content: '...'},
+  { data: { year: '2017', tag: 'three', month: 'feb', day: '02'}, content: '...'},
+  { data: { year: '2017', tag: 'three', month: 'feb', day: '02'}, content: '...'}
+]
+
+var res = groupBy(arr, 'data.year', 'data.tag', 'data.month', 'data.day');
+console.log(util.inspect(res, {depth: null}));
